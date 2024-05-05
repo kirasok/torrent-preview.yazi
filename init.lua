76970b6f9d6f3031e9cd57c8595a53e9f9f48c18
Kira Sokolova <Kyra256@proton.me>
2024-05-06 02:00:32 +0300

    feat: implement scrolling

--- init.lua
+++ init.lua
@@ -1,19 +1,53 @@
 local M = {}
 
 function M:peek()
-	local output, code = Command("transmission-show")
+	-- launch process
+	local process, code = Command("transmission-show")
 		:args({
 			tostring(self.file.url),
 		})
 		:stdout(Command.PIPED)
-		:output()
+		:spawn()
 
-	local p
-	p = ui.Paragraph.parse(self.area, output.stdout)
+	local limit = self.area.h
+	-- read and count lines from process
+	local i, lines = 0, ""
+	repeat
+		local next, event = process:read_line()
+		if event ~= 0 then
+			break
+		end
 
-	ya.preview_widgets(self, { p:wrap(ui.Paragraph.WRAP) })
+		i = i + 1
+		-- only concatenate lines that are past 'skip' number of STDOUT
+		if i > self.skip then
+			lines = lines .. next
+		end
+	until i >= self.skip + limit -- until reader reaches max number of lines on screen plus skip
+
+	process:start_kill()
+
+	-- if paged below all output, run peek again with smaller skip
+	if self.skip > 0 and i < self.skip + limit then
+		ya.manager_emit(
+			"peek",
+			{ tostring(math.max(0, i - limit)), only_if = tostring(self.file.url), upper_bound = "" }
+		)
+	-- preview torrent
+	else
+		ya.preview_widgets(self, { ui.Paragraph.parse(self.area, lines):wrap(ui.Paragraph.WRAP) })
+	end
 end
 
-function M:seek() end
+function M:seek(units)
+	local h = cx.active.current.hovered
+	if h and h.url == self.file.url then
+		local step = math.floor(units * self.area.h / 10)
+		ya.manager_emit("peek", {
+			math.max(0, cx.active.preview.skip + step),
+			only_if = self.file.url,
+		})
+	end
+end
 
 return M
