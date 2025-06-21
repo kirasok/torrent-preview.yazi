05c5931c012f6d999c80f1aa8ffb72ae90869614
Kira Sokolova <Kyra256@proton.me>
2025-06-21 20:26:38 +0300

    fix(#4): use proper variable to get tab_size

--- main.lua
+++ main.lua
@@ -33,7 +33,7 @@ function M:peek(job)
 	if job.skip > 0 and i < job.skip + limit then
 		ya.manager_emit("peek", { math.max(0, i - limit), only_if = job.file.url, upper_bound = true })
 	else
-		lines = lines:gsub("\t", string.rep(" ", PREVIEW.tab_size))
+		lines = lines:gsub("\t", string.rep(" ", rt.preview.tab_size))
 		ya.preview_widget(job, { ui.Text.parse(lines):area(job.area) })
 	end
 end
